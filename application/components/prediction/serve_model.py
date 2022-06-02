f64ea45d2b43a05aa8e8546695a673cceb96dfcf
adwait1291 <76807214+adwait1291@users.noreply.github.com>
2022-06-02 20:45:20 +0530

    Done
    Done

--- application/components/prediction/serve_model.py
+++ application/components/prediction/serve_model.py
@@ -3,35 +3,46 @@ from io import BytesIO
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from tensorflow.keras.applications.imagenet_utils import decode_predictions
-
+#from tensorflow.keras.applications.imagenet_utils import decode_predictions
+from keras.models import load_model
+MODEL_PATH = 'application/Assets/anomoly.h5'
 model = None
 
 
-def load_model():
-    model = tf.keras.applications.MobileNetV2(weights="imagenet")
+def loadModel():
+    model = load_model(MODEL_PATH)
     print("Model loaded")
     return model
+
+def check_anomaly(img):
+    reconstruction_error_threshold = 0.0035
+    img = np.array(img.resize((224,224), Image.ANTIALIAS))
+    img = img / 255.
+    img = img[np.newaxis, :,:,:]
+    reconstruction = model.predict([[img]])
+    reconstruction_error = model.evaluate([reconstruction],[[img]], batch_size = 1)[0]
+
+    if reconstruction_error > reconstruction_error_threshold:
+        return "Anomaly" 
+    else:
+        return "NotAnAnomaly"
 
 
 def predict(image: Image.Image):
     global model
     if model is None:
-        model = load_model()
+        model = loadModel()
 
-    image = np.asarray(image.resize((224, 224)))[..., :3]
-    image = np.expand_dims(image, 0)
-    image = image / 127.5 - 1.0
+    #image = np.asarray(image.resize((224, 224)))[..., :3]
+    #image = np.expand_dims(image, 0)
+    #image = image / 255.0
 
-    result = decode_predictions(model.predict(image), 2)[0]
+    result = check_anomaly(image)
 
     response = []
-    for i, res in enumerate(result):
-        resp = {}
-        resp["class"] = res[1]
-        resp["confidence"] = f"{res[2]*100:0.2f} %"
-
-        response.append(resp)
+    resp = {}
+    resp["Result"] = result
+    response.append(resp)
 
     return response
 
--- application/server/main.py
+++ application/server/main.py
@@ -3,11 +3,8 @@ from fastapi import FastAPI, File, UploadFile
 from starlette.responses import RedirectResponse
 
 from application.components import predict, read_imagefile
-from application.schema import Symptom
-from application.components.prediction import symptom_check
 
 app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
-<h2>Try Covid symptom checker api - it is just a learning app demo</h2>
 <br>by Aniket Maurya"""
 
 app = FastAPI(title='Tensorflow FastAPI Starter Pack', description=app_desc)
@@ -29,9 +26,7 @@ async def predict_api(file: UploadFile = File(...)):
     return prediction
 
 
-@app.post("/api/covid-symptom-check")
-def check_risk(symptom: Symptom):
-    return symptom_check.get_risk_level(symptom)
+
 
 
 if __name__ == "__main__":
