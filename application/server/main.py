24c3324794a9fa78a711e3845b0608818d5d3405
Adwait Kesharwani <adwait1291@gmail.com>
2022-06-17 18:28:03 +0530

    Update main.py

--- application/server/main.py
+++ application/server/main.py
@@ -14,9 +14,6 @@ async def index():
 
 @app.post("/predict/image")
 async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
-    if not extension:
-        return "Image must be jpg or png format!"
     image = read_imagefile(await file.read())
     prediction = predict(image)
     return prediction
